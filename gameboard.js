fe43232a41dbf8a53098f1ec5902ad6ef720c092
Clark Alesna <clark_alesna@hotmail.com>
2020-02-10 00:39:02 +0800

    implemented full tictactoe logic

--- gameboard.js
+++ gameboard.js
@@ -1,22 +1,205 @@
 class Gameboard {
-    width = 3;
-    height = 3;
+    size = 3;
     cellWidth = 266.67;
     cellHeight = 266.67;
     gameContainer = null;
+    // 0 = Player One, 1 = Player Two
+    currentPlayer = 0;
+    playerSymbols = [
+        "X",
+        "O"
+    ];
+    cells = [];
+    isStarted = false;
 
     initialize() {
         this.gameContainer = document.querySelector("#game-container");
 
-        for (let x = 0; x < this.width; x++) {
-            for (let y = 0; y < this.height; y++) {
+        for (let y = 0; y < this.size; y++) {
+            for (let x = 0; x < this.size; x++) {
                 let newCell = new GameCell(this.cellWidth, this.cellHeight, x, y);
                 this.appendCell(newCell);
+
+                if (!this.cells[x])
+                    this.cells[x] = [];
+
+                this.cells[x][y] = newCell;
+
+                newCell.onActivate = this.onCellActivated;
             }
         }
+        //Temporary
+        this.isStarted = true;
+        console.log(this.cells);
     }
 
     appendCell(cell) {
         this.gameContainer.appendChild(cell.Element);
+    }
+
+    onCellActivated = (cell) => {
+        if (this.isStarted) {
+            cell.setValue(this.playerSymbols[this.currentPlayer]);
+            this.checkForWinner();
+            this.checkDraw();
+            this.currentPlayer++;
+            if (this.currentPlayer >= this.playerSymbols.length) {
+                this.currentPlayer = 0;
+            }
+        }
+    }
+
+    checkForWinner = () => {
+        let result = false;
+        for (let y = 0; y < this.size; y++) {
+            for (let x = 0; x < this.size; x++) {
+                let currentCell = this.cells[x][y];
+
+                result =
+                    this.checkForHorizontalWin(currentCell) ||
+                    this.checkForVerticalWin(currentCell) ||
+                    this.checkForDiagonalWin(currentCell);
+
+                this.checkResult(result);
+                if (result) break;
+            }
+            if (result) break;
+        }
+    }
+
+    checkForHorizontalWin = (cell) => {
+        let result = false;
+        let symbol = cell.TextContainer.innerText;
+
+        if (symbol.length > 0) {
+
+            let foundCtr = 1;
+            // Check for Left
+            for (let x = cell.x - 1; x >= 0; x--) {
+                let currentCell = this.cells[x][cell.y];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+
+            // Check for Right
+            for (let x = cell.x + 1; x < this.size; x++) {
+                let currentCell = this.cells[x][cell.y];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+            if (foundCtr >= this.size)
+                result = true;
+
+        }
+        return result;
+    }
+
+
+    checkForVerticalWin = (cell) => {
+        let result = false;
+        let symbol = cell.TextContainer.innerText;
+
+        if (symbol.length > 0) {
+
+            let foundCtr = 1;
+
+            // Check for Up
+            for (let y = cell.y - 1; y >= 0; y--) {
+                let currentCell = this.cells[cell.x][y];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+
+            // Check for Down
+            for (let y = cell.y + 1; y < this.size; y++) {
+                let currentCell = this.cells[cell.x][y];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+            if (foundCtr >= this.size)
+                result = true;
+
+        }
+
+        return result;
+    }
+
+    checkForDiagonalWin = (cell) => {
+
+        let result = false;
+        let symbol = cell.TextContainer.innerText;
+
+        if (symbol.length > 0) {
+
+            let foundCtr = 1;
+            // Check for UpLeft
+            let y = cell.y - 1;
+            let x = cell.x - 1;
+            while (y >= 0 && x >= 0) {
+                let currentCell = this.cells[x--][y--];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+
+
+            foundCtr = 1;
+            // Check for DownRight
+            y = cell.y + 1;
+            x = cell.x + 1;
+            while (y < this.size && x < this.size) {
+                let currentCell = this.cells[x++][y++];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+
+            foundCtr = 1;
+            // Check for UpRight
+            y = cell.y - 1;
+            x = cell.x + 1;
+            while (y >= 0 && x < this.size) {
+                let currentCell = this.cells[x++][y--];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+
+            foundCtr = 1;
+            // Check for DownLeft
+            y = cell.y + 1;
+            x = cell.x - 1;
+            while (y < this.size && x >= 0) {
+                let currentCell = this.cells[x--][y++];
+                var currentSymbol = currentCell.TextContainer.innerText;
+                if (currentSymbol == symbol) foundCtr++;
+            }
+
+
+            if (foundCtr >= this.size)
+                result = true;
+
+        }
+        return result;
+    }
+
+    checkDraw = () => {
+        let isDraw = true;
+        for (let y = 0; y < this.size; y++) {
+            for (let x = 0; x < this.size; x++) {
+                console.log(`Is Active ${x}, ${y}  = ${this.cells[x][y].isActive}`);
+                if (!this.cells[x][y].isActive) isDraw = false;
+            }
+        }
+
+        console.log("Judgment", isDraw);
+        if (isDraw) {
+            this.isStarted = false;
+            alert("Draw!!!");
+        }
+    }
+
+    checkResult = (r) => {
+        if (r) {
+            alert(`Winner is Player: ${this.currentPlayer + 1}`);
+            this.isStarted = false;
+        }
     }
 }
--- gamecell.js
+++ gamecell.js
@@ -6,6 +6,8 @@ class GameCell {
     absoluteX = 0;
     absoluteY = 0;
     Element = null;
+    TextContainer = null;
+    isActive = false;
 
     constructor(w, h, x, y) {
         this.width = w;
@@ -15,7 +17,17 @@ class GameCell {
         this.absoluteX = w * x;
         this.absoluteY = h * y;
         let newCell = document.createElement("div");
+        let textContainer = document.createElement("span");
+
+        textContainer.style.fontSize = "187px";
+        textContainer.style.color = "white";
+        textContainer.style.verticalAlign = "middle";
+        textContainer.style.display = "inline-block";
+        textContainer.style.marginTop = "calc(50% - 224px / 2)";
+
+        this.TextContainer = textContainer;
         newCell.classList.add("cell");
+        newCell.appendChild(textContainer);
 
         // Set CSS Properties
         newCell.style.width = `${this.width}px`;
@@ -24,11 +36,23 @@ class GameCell {
         newCell.style.position = "absolute";
         newCell.style.left = `${(this.width * x)}px`;
         newCell.style.top = `${(this.height * y)}px`;
+        newCell.style.textAlign = "center";
         newCell.addEventListener("click", this.onElementClicked);
         this.Element = newCell;
     }
 
     onElementClicked = () => {
-        console.log(this);
+        if (this.onActivate != null) {
+            this.onActivate(this);
+        }
     }
+
+    setValue = (v) => {
+        if (!this.isActive) {
+            this.isActive = true;
+            this.TextContainer.innerText = v;
+        }
+    };
+
+    onActivate = null;
 }
